aa3508ff70af39358e22dc4846390c5dc028e22e
andre91y <53210904+andre91y@users.noreply.github.com>
2019-09-02 17:24:59 +0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (<header className='header'>
+        <img src='https://minecraft-statistic.net/img/screen/icon/103354.png' />
+    </header>);
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (<div className='content'>
+        <div>
+            <img src='https://avatars.mds.yandex.net/get-pdb/1766868/85193472-d20f-4d23-9022-4601de220196/s1200' />
+        </div>
+        <div>
+            ava + description
+    </div>
+        <div>
+            My Post
+      <div>
+                New post
+      </div>
+            <div>
+                <div>
+                    post 1
+        </div>
+                <div>
+                    post 2
+        </div>
+            </div>
+        </div>
+
+
+    </div>);
+}
+export default Profile;
